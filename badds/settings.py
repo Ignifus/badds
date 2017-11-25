2d113849cf99079fe87a0a97f5874db4170232c5
Patricio Napoli <master@ignifus.com>
2017-11-25 15:50:12 -0300

    Allowed all hosts.

--- badds/settings.py
+++ badds/settings.py
@@ -25,7 +25,7 @@ if "BADDS_SECRET" in os.environ:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
