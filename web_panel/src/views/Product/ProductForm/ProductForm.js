dd137357c081e8d1f71682b64f1c85a5bb7cacc7
nicolaseguez <nicolaseguez>
2019-11-19 08:04:19 -0300

    starting product form

--- web_panel/src/views/Product/ProductForm/ProductForm.js
+++ web_panel/src/views/Product/ProductForm/ProductForm.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/styles';
-import { Grid } from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
+import { Button, Divider, Grid, Typography, TextField } from '@material-ui/core';
 
 import { ProductsToolbar } from '../components';
 
-const useStyles = makeStyles(theme => ({
+const styles = (theme => ({
   root: {
     padding: theme.spacing(3)
   },
@@ -13,22 +13,60 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const ProductForm = () => {
-  const classes = useStyles();
+class ProductFormBase extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  return (
-    <div className={classes.root}>
-      <ProductsToolbar showSearch={false} />
-      <div className={classes.content}>
-        <Grid
-          container
-          spacing={3}
-        >
-          <h1>form</h1>
-        </Grid>
+  handleSubmit(e) {
+    e.preventDefault();
+    alert("caca");
+  }
+
+  render () {
+    const {classes} = this.props;
+
+    return (
+      <div className={classes.root}>
+        <ProductsToolbar showSearch={false} />
+        <div className={classes.content}>
+          <Grid
+            container
+            spacing={3}
+          >
+            <Grid item xs={12}>
+              <Typography variant="h2" component="h2">Form</Typography>
+              <Divider />
+            </Grid>
+            <Grid item xs={12}>
+              <form onSubmit={this.handleSubmit}>
+                <Grid container spacing={2}>
+                  <Grid item xs={4}>
+                    <TextField label="Name" placeholder="Nombre de la app" fullWidth />
+                  </Grid>
+                  <Grid item xs={4}>
+                    <TextField label="Name" placeholder="Nombre de la app" fullWidth />
+                  </Grid>
+                  <Grid item xs={4}>
+                    <TextField label="Name" placeholder="Nombre de la app" fullWidth />
+                  </Grid>
+                </Grid>
+                <Grid container>
+                  <Grid item>
+                    <Button type="submit">Submit</Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </Grid>
+          </Grid>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
+
+const ProductForm = withStyles(styles)(ProductFormBase);
 
 export { ProductForm };
