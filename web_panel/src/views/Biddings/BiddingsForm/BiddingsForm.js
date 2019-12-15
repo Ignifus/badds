ca05e8495229ddec3d96d425f1f4415e3a495394
nicolaseguez <nicolaseguez>
2019-12-14 23:38:38 -0300

    bidding form

--- web_panel/src/views/Biddings/BiddingsForm/BiddingsForm.js
+++ web_panel/src/views/Biddings/BiddingsForm/BiddingsForm.js
@@ -12,6 +12,11 @@ import {
   LinearProgress,
   FormHelperText,
   MenuItem,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Divider
 } from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
 import validate from 'validate.js';
@@ -47,10 +52,20 @@ class BiddingFormBase extends React.Component {
       this.setState({ errors: [] });
     }
 
-    if (this.props.match.params.id == null) {
-      this.props.createBidding(this.state);
+    const {id, auctionId} = this.props.match.params;
+
+    if (id == null) {
+      this.props.createBidding({
+        auction: auctionId,
+        ppp_usd: this.state.ppp_usd,
+        advertisement: this.state.advertisement
+      });
     } else {
-      this.props.updateBidding(this.state.id, this.state)
+      this.props.updateBidding(this.state.id, {
+        auction: auctionId,
+        ppp_usd: this.state.ppp_usd,
+        advertisement: this.state.advertisement
+      })
     }
   }
 
@@ -110,63 +125,126 @@ class BiddingFormBase extends React.Component {
       </Grid>)
     }
 
+    let detail = null;
+    if (auction) {
+      detail = this.renderDetail();
+    }
+
     if (success && this.props.match.params.id == null) {
       this.reset();
     }
     // TODO agregar datos de la compra
     return (
-      <form onSubmit={this.handleSubmit} noValidate>
-        {
-          hasError && <FailedSnackbar message="Tuvimos un problema al procesar su peticion" />
-        }
-        {
-          success && <SuccessSnackbar message="Operacion concluida con exito" />
-        }
-        <Grid container spacing={2}>
-          {progressBar}
-          <Grid item xs={4}>
-            <FormControl fullWidth>
-              <TextField label="PPP USD"
-                name="ppp_usd"
-                type="number"
-                placeholder="Pago por Impresion"
-                value={this.state.ppp_usd}
-                error={this.state.errors.ppp_usd != null}
-                helperText={this.state.errors.ppp_usd != null ? this.state.errors.ppp_usd[0] : ''}
-                onChange={this.handleChange}
-                required
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={4}>
-            <FormControl fullWidth>
-              <InputLabel id="badds-app-category-select">Aviso</InputLabel>
-              <Select
-                labelId="badds-app-category-select"
-                value={this.state.advertisement}
-                onChange={this.handleChange}
-                error={this.state.errors.advertisement != null}
-                name="advertisement"
-                required
-              >
-                {
-                  ads.map(ad => <MenuItem key={ad.id} value={ad.id}>{ad.name}</MenuItem>)
-                }
-              </Select>
-              <FormHelperText error>{this.state.errors.advertisement != null ? this.state.errors.advertisement[0] : ''}</FormHelperText>
-            </FormControl>
-          </Grid>
+      <Grid container>
+        {detail}
+        <Grid item xs={12}>
+          <Typography variant="h4">Datos de confirmacion</Typography>
         </Grid>
-        <Grid container>
-          <Grid item>
-            <Button
-              type="submit"
-              color="primary"
-              style={{marginTop: '30px'}}>Submit</Button>
-          </Grid>
+        <Grid item xs={12}>
+          <form onSubmit={this.handleSubmit} noValidate>
+            {
+              hasError && <FailedSnackbar message="Tuvimos un problema al procesar su peticion" />
+            }
+            {
+              success && <SuccessSnackbar message="Operacion concluida con exito" />
+            }
+            <Grid container spacing={2}>
+              {progressBar}
+              <Divider />
+              <Grid item xs={4}>
+                <FormControl fullWidth>
+                  <TextField label="PPP USD"
+                    name="ppp_usd"
+                    type="number"
+                    placeholder="Pago por Impresion"
+                    value={this.state.ppp_usd}
+                    error={this.state.errors.ppp_usd != null}
+                    helperText={this.state.errors.ppp_usd != null ? this.state.errors.ppp_usd[0] : ''}
+                    onChange={this.handleChange}
+                    required
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={4}>
+                <FormControl fullWidth>
+                  <InputLabel id="badds-app-category-select">Aviso</InputLabel>
+                  <Select
+                    labelId="badds-app-category-select"
+                    value={this.state.advertisement}
+                    onChange={this.handleChange}
+                    error={this.state.errors.advertisement != null}
+                    name="advertisement"
+                    required
+                  >
+                    {
+                      ads.map(ad => <MenuItem key={ad.id} value={ad.id}>{ad.name}</MenuItem>)
+                    }
+                  </Select>
+                  <FormHelperText error>{this.state.errors.advertisement != null ? this.state.errors.advertisement[0] : ''}</FormHelperText>
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Grid container>
+              <Grid item>
+                <Button
+                  type="submit"
+                  color="primary"
+                  variant="contained"
+                  style={{marginTop: '30px'}}>Confirmar Compra</Button>
+              </Grid>
+            </Grid>
+          </form>
         </Grid>
-      </form>
+      </Grid>
     );
+  }
+
+  renderDetail() {
+    const { auction } = this.props;
+
+    return (<Grid item xs={12}>
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography variant="h4">Detalles de compra</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography component="strong">Detalles</Typography>
+          <List>
+            <ListItem>
+              <ListItemText
+                primary="Nombr de la App"
+                secondary={auction.space.application.name}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary="Dominio"
+                secondary={auction.space.application.domain}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary="Dimensiones"
+                secondary={`${auction.space.x_size} x ${auction.space.y_size}`}
+              />
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography component="strong">Restricciones</Typography>
+          <List>
+            {
+              auction.space.restrictions.map(restriction => (<ListItem key={restriction.restriction__restriction}>
+                <ListItemText
+                  primary={restriction.restriction__restriction}
+                  secondary={restriction.value}
+                />
+              </ListItem>))
+            }
+          </List>
+        </Grid>
+      </Grid>
+    </Grid>);
   }
 };
 
