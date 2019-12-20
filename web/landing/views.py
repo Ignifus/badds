1d848ac7c0d8a840aba80891daedaaaacb1ff07b
Patricio Napoli <pnapoli@geminis.io>
2019-12-20 00:37:45 -0300

    Added space restriction filtering on ads.

--- web/landing/views.py
+++ web/landing/views.py
@@ -5,7 +5,7 @@ import os
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
-from django.http import HttpResponse, FileResponse
+from django.http import FileResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
 
